9f524f6a769cb34b3a9b8ecb13a9098270a33fcf
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-07-31 08:51:20 -0300

    Update Sobre.js
    Arrumei um componente do Reactstrap que não funcionava

--- src/Sobre.js
+++ src/Sobre.js
@@ -1,24 +1,89 @@
 import React, { useState } from "react";
-import { Button, Fade } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 
-export default function sobre (props) {
+export default function sobre(props) {
 
-    const [fadeIn, setFadeIn] = useState(true);
-    const toggle = () => setFadeIn(!fadeIn);
 
-    return(
+
+    return (
         <>
 
             <p id="teste"> teste teste oiiiii ai ai i iaiaifiii i i iogh 423htui3q rhwh </p>
-            <div>
-                <Button color="primary" onClick={toggle}>Toggle Fade</Button>
-                <Fade in={fadeIn} tag="h5" className="mt-3">
-                    asdf.
-                </Fade>
-            </div>
+
+            <Form>
+                <FormGroup>
+                    <Label for="exampleEmail">Email</Label>
+                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="examplePassword">Password</Label>
+                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="exampleSelect">Select</Label>
+                    <Input type="select" name="select" id="exampleSelect">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Input>
+                </FormGroup>
+                <FormGroup>
+                    <Label for="exampleSelectMulti">Select Multiple</Label>
+                    <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Input>
+                </FormGroup>
+                <FormGroup>
+                    <Label for="exampleText">Text Area</Label>
+                    <Input type="textarea" name="text" id="exampleText" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="exampleFile">File</Label>
+                    <Input type="file" name="file" id="exampleFile" />
+                    <FormText color="muted">
+                        This is some placeholder block-level help text for the above input.
+                        It's a bit lighter and easily wraps to a new line.
+                    </FormText>
+                </FormGroup>
+                <FormGroup tag="fieldset">
+                    <legend>Radio Buttons</legend>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="radio" name="radio1" />{' '}
+                            Option one is this and that—be sure to include why it's great
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="radio" name="radio1" />{' '}
+                            Option two can be something else and selecting it will deselect option one
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check disabled>
+                        <Label check>
+                            <Input type="radio" name="radio1" disabled />{' '}
+                            Option three is disabled
+                        </Label>
+                    </FormGroup>
+                </FormGroup>
+                <FormGroup check>
+                    <Label check>
+                        <Input type="checkbox" />{' '}
+                        Check me out
+                    </Label>
+                </FormGroup>
+                <Button>Submit</Button>
+            </Form>
+
 
         </>
-)
+    )
 
 }
