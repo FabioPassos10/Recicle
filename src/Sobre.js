8b6d3a881120a4eaa4e109bd787d6d547979c2e1
luan <60790140+luaaan@users.noreply.github.com>
2021-07-30 22:13:04 -0300

    Update Sobre.js
    nuu

--- src/Sobre.js
+++ src/Sobre.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Fade } from 'reactstrap';
 
 
+export default function sobre (props) {
 
-export default function sobre (props){
+    const [fadeIn, setFadeIn] = useState(true);
+    const toggle = () => setFadeIn(!fadeIn);
 
-return(
-    <>
+    return(
+        <>
 
-    <p style="color: red;"> teste teste oiiiii ai ai i iaiaifiii i i iogh 423htui3q rhwh </p>
+            <p id="teste"> teste teste oiiiii ai ai i iaiaifiii i i iogh 423htui3q rhwh </p>
+            <div>
+                <Button color="primary" onClick={toggle}>Toggle Fade</Button>
+                <Fade in={fadeIn} tag="h5" className="mt-3">
+                    asdf.
+                </Fade>
+            </div>
 
-    
-    
-    </>
+        </>
 )
 
 }
