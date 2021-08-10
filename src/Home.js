1a345e7b76ab52373723faf32e21af02bae1d96a
Alexandre2DS <alexandrebsz158@gmail.com>
2021-08-10 20:37:31 -0300

    Update Home.js
    corrigiu erro

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-<<<<<<< Updated upstream
 
 export default function Home (){
 return(
@@ -15,14 +14,3 @@ return(
 
 )
 }
-=======
-export default function Home() {
-  return (
-    <>
-      <p>estamos no Home</p>
-      <p>de fato, estamos no home</p>
-      <p>Eu disse que estavmos no Home</p>
-    </>
-  );
-}
->>>>>>> Stashed changes
