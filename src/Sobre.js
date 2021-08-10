ed6eb39a706a906b96971e50d5ed471da58f0527
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-08-10 20:46:33 -0300

    Update Sobre.js
    Retirei os componentes de teste

--- src/Sobre.js
+++ src/Sobre.js
@@ -1,17 +1,8 @@
 import React, { useState } from "react";
-<<<<<<< Updated upstream
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-=======
-import { Button, Fade } from "reactstrap";
->>>>>>> Stashed changes
+
+
 
 export default function sobre(props) {
-  const [fadeIn, setFadeIn] = useState(true);
-  const toggle = () => setFadeIn(!fadeIn);
-
-<<<<<<< Updated upstream
-export default function sobre(props) {
-
 
 
     return (
@@ -19,98 +10,8 @@ export default function sobre(props) {
 
             <p id="teste"> teste teste oiiiii ai ai i iaiaifiii i i iogh 423htui3q rhwh </p>
 
-            <Form>
-                <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSelect">Select</Label>
-                    <Input type="select" name="select" id="exampleSelect">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSelectMulti">Select Multiple</Label>
-                    <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleText">Text Area</Label>
-                    <Input type="textarea" name="text" id="exampleText" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleFile">File</Label>
-                    <Input type="file" name="file" id="exampleFile" />
-                    <FormText color="muted">
-                        This is some placeholder block-level help text for the above input.
-                        It's a bit lighter and easily wraps to a new line.
-                    </FormText>
-                </FormGroup>
-                <FormGroup tag="fieldset">
-                    <legend>Radio Buttons</legend>
-                    <FormGroup check>
-                        <Label check>
-                            <Input type="radio" name="radio1" />{' '}
-                            Option one is this and that—be sure to include why it's great
-                        </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                        <Label check>
-                            <Input type="radio" name="radio1" />{' '}
-                            Option two can be something else and selecting it will deselect option one
-                        </Label>
-                    </FormGroup>
-                    <FormGroup check disabled>
-                        <Label check>
-                            <Input type="radio" name="radio1" disabled />{' '}
-                            Option three is disabled
-                        </Label>
-                    </FormGroup>
-                </FormGroup>
-                <FormGroup check>
-                    <Label check>
-                        <Input type="checkbox" />{' '}
-                        Check me out
-                    </Label>
-                </FormGroup>
-                <Button>Submit</Button>
-            </Form>
-
-
+           
         </>
     )
 
 }
-=======
-  return (
-    <>
-      <p id="teste">
-        {" "}
-        teste teste oiiiii ai ai i iaiaifiii i i iogh 423htui3q rhwh{" "}
-      </p>
-      <div>
-        <Button color="primary" onClick={toggle}>
-          Toggle Fade
-        </Button>
-        <Fade in={fadeIn} tag="h5" className="mt-3">
-          asdf.
-        </Fade>
-      </div>
-    </>
-  );
-}
->>>>>>> Stashed changes
