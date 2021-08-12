8316e0d485fe674e5a8064028e5f62c6082c9d74
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-08-12 19:29:19 -0300

    Update Sobre.js
    testando

--- src/Sobre.js
+++ src/Sobre.js
@@ -7,7 +7,7 @@ export default function sobre(props) {
 
     return (
         <>
-
+            <p>testando<p/>
             <p id="teste"> teste teste oiiiii ai ai i iaiaifiii i i iogh 423htui3q rhwh </p>
             <p>:)</p>
 
