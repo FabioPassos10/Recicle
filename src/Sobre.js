0cf2616988d52eee8f8ab88a8531912d7c230152
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-09-07 11:39:35 -0300

    Update Sobre.js

--- src/Sobre.js
+++ src/Sobre.js
@@ -1,17 +1,6 @@
 import React from "react";
-let map;
 
-function initMap() {
-    map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-}
 export default function sobre(props) {
-
-
-
-
     return (
         <section>
             <p>
