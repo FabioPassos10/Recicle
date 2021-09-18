70be359152bdb5c6646771d48b38154e44324b8b
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-09-18 14:27:52 -0300

    Update logo.js
    deixei o logo provisoriamente comentado

--- src/Componentes/logo.js
+++ src/Componentes/logo.js
@@ -1,16 +1,17 @@
-import React from "react";
-import logo from "../assets/logo.png";
+// import React from "react";
+// import logo from "../assets/logo.png";
+// import "./Styles/logo.css";
 
-function Logo() {
-  return (
-    <img
-      src={logo}
-      alt="logo"
-      width="32px"
-      height="32px"
-      style={{ marginRight: "1.5em" }}
-    />
-  );
-}
+// function Logo() {
+//   return (
+//     <img
+//       src={logo}
+//       alt="logo"
+//       width="32px"
+//       height="32px"
+//       style={{ marginRight: "1.5em" }}
+//     />
+//   );
+// }
 
-export default Logo;
+// export default Logo;
