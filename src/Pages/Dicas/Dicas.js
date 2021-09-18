d6dd12f45e88afc4daeebcd26cb394814c796b35
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-09-18 14:30:10 -0300

    Create Dicas.js

---
+++ src/Pages/Dicas/Dicas.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Dicas() {
+  return (
+    <section>
+      <p>tst</p>
+      <p>Em Desenvolvimento...</p>
+      <p>oi</p>
+      <p>kkk</p>
+    </section>
+  );
+}
