a49684e116630b491cb6ced41db41c685844ad5b
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-09-18 14:29:41 -0300

    Create Home.js

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Cards from "../../Componentes/Card";
+import { Container, Row, Col } from "reactstrap";
+import Mapa from "../../Componentes/Map";
+import "./home.css";
+
+export default function Home(props) {
+  return (
+    <>
+      <Container id="Hero" fluid={true}></Container>
+      <div id="Mapa">
+        {/* {" "}
+        <Mapa></Mapa>{" "} */}
+      </div>
+
+      <div id="DivCard_Home"></div>
+    </>
+  );
+}
