9d9f62b130fc0361276b42ba990a12cc1854d911
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-09-18 17:05:52 -0300

    Update Card.js

--- src/Componentes/Card.js
+++ src/Componentes/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Styles/card.css";
 
 import {
   Card,
@@ -12,66 +13,34 @@ import {
 } from "reactstrap";
 const Example = (props) => {
   return (
-    <CardGroup>
-      <Card>
-        <CardImg
-          top
-          width="100%"
-          src="/assets/318x180.svg"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">
-            Card subtitle
-          </CardSubtitle>
-          <CardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg
-          top
-          width="100%"
-          src="/assets/318x180.svg"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">
-            Card subtitle
-          </CardSubtitle>
-          <CardText>
-            This card has supporting text below as a natural lead-in to
-            additional content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg
-          top
-          width="100%"
-          src="/assets/318x180.svg"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">
-            Card subtitle
-          </CardSubtitle>
-          <CardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-    </CardGroup>
+    <>
+      <div id="Card1">
+        <div>
+          <p>
+            Lorem ipsum primis vestibulum pretium viverra porttitor potenti, ut
+            inceptos elementum aenean netus orci mi eget, dolor fringilla
+            faucibus himenaeos nisi mauris. class quisque hendrerit leo eleifend
+            neque pretium sit aenean,
+          </p>
+        </div>
+      </div>
+      <div id="Card2">
+        <div>
+          Lorem ipsum primis vestibulum pretium viverra porttitor potenti, ut
+          inceptos elementum aenean netus orci mi eget, dolor fringilla faucibus
+          himenaeos nisi mauris. class quisque hendrerit leo eleifend neque
+          pretium sit aenean,
+        </div>
+      </div>
+      <div id="Card3">
+        <div>
+          Lorem ipsum primis vestibulum pretium viverra porttitor potenti, ut
+          inceptos elementum aenean netus orci mi eget, dolor fringilla faucibus
+          himenaeos nisi mauris. class quisque hendrerit leo eleifend neque
+          pretium sit aenean,
+        </div>
+      </div>
+    </>
   );
 };
 
