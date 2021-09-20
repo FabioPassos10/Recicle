8d84a68ffd15078b1f748e6814acfba0226633f3
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-09-20 09:49:34 -0300

    Update Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,19 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Cards from "../../Componentes/Card";
-import { Container, Row, Col } from "reactstrap";
+import "../../Componentes/Styles/card.css";
+import {
+  Container,
+  Row,
+  Col,
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 import Mapa from "../../Componentes/Map";
 import "./home.css";
 import Card from "../../Componentes/Card";
+
 export default function Home(props) {
   return (
     <>
@@ -14,7 +24,39 @@ export default function Home(props) {
       </div>
       <div id="Triangulo"></div>
       <div id="DivCard_Home">
-        <div id="CardDent">{/* <Card></Card> */}</div>
+        <div id="CardDent">
+          <div id="divPrin">
+            <div id="CardPapel">
+              <div id="abPapel"></div>
+              <h1>Papel</h1>
+            </div>
+
+            <div id="CardPlastico">
+              <div id="abPlastico"></div>
+              <h1>Plastico</h1>
+            </div>
+
+            <div id="CardVidro">
+              <div id="abVidro"></div>
+              <h1>Vidro</h1>
+            </div>
+
+            <div id="CardOrganico">
+              <div id="abOrganico"></div>
+              <h1>Organico</h1>
+            </div>
+
+            <div id="CardMetal">
+              <div id="abMetal"></div>
+              <h1>Metal</h1>
+            </div>
+
+            <div id="CardEletronico">
+              <div id="abEletronico"></div>
+              <h1>Eletronico</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
