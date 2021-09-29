360e01451a7d01b204f1af53b5cefd3eb529b88e
Aluno <>
2021-09-29 07:39:09 -0300

    Update Sobre.js
    teste

--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -3,68 +3,7 @@ import React from "react";
 export default function sobre(props) {
   return (
     <section>
-      {/* <p>
-                Lorem ipsum primis vestibulum pretium viverra porttitor potenti, ut inceptos elementum aenean netus orci mi eget, dolor fringilla faucibus himenaeos nisi mauris.
-                class quisque hendrerit leo eleifend neque pretium sit aenean, euismod praesent placerat potenti tempus suscipit lacinia, ullamcorper curabitur mattis curabitur congue aliquam turpis.
-                nunc lectus libero nam varius quisque libero elit imperdiet habitasse curabitur gravida rutrum, fusce nostra ut orci in vitae iaculis consequat integer amet.
-                integer curae malesuada interdum non interdum lorem bibendum vivamus, porttitor urna fames consequat porta nostra viverra ornare, lacus amet scelerisque integer vestibulum sed blandit.
-            </p>
-            <p>
-                Nisl sociosqu suspendisse magna risus eros primis etiam arcu turpis, commodo sociosqu mauris velit eros donec nam cras eleifend, consectetur urna arcu viverra neque cras praesent velit.
-                lobortis torquent ornare litora torquent convallis lectus feugiat, ornare vulputate velit volutpat justo proin nostra nibh, gravida elementum lobortis blandit metus nullam.
-                egestas aliquam litora aliquam iaculis tempor etiam elit facilisis felis, interdum pellentesque venenatis auctor porttitor donec porttitor enim eleifend, leo in nec ut mauris massa commodo purus.
-                fringilla hendrerit scelerisque condimentum curabitur phasellus commodo dolor venenatis, quis convallis velit nullam sem etiam praesent urna accumsan, malesuada arcu lacinia justo semper nibh class.
-            </p>
-            <p>
-                Mauris volutpat varius maecenas erat congue dolor egestas cras, euismod risus volutpat aenean ante feugiat felis rutrum quisque, etiam cras vel lorem mattis nec neque.
-                maecenas rhoncus semper elit cubilia sapien mauris, a sed ligula sagittis libero urna ut, hac commodo porttitor phasellus ut.
-                vitae sed velit cubilia commodo, dapibus porttitor eros elementum volutpat, sollicitudin nec suspendisse.
-                sapien ut netus iaculis quis cubilia semper platea aenean sed lectus feugiat, platea vivamus auctor condimentum semper aenean per lectus mauris convallis, donec aenean tempus tortor leo pharetra conubia magna odio ante.
-                tincidunt adipiscing erat cubilia class quisque molestie pretium torquent, per luctus feugiat interdum taciti dolor fames erat convallis, scelerisque nec habitasse nam orci ac erat.
-            </p>
-            <p>
-                Cras erat sapien interdum nostra eu pellentesque phasellus volutpat libero eros at maecenas vitae, neque vel amet per ultrices eu himenaeos faucibus primis sapien suscipit.
-                felis ut dolor posuere malesuada rhoncus elementum nec pharetra pretium rhoncus, mi viverra vivamus fames justo velit himenaeos eget lorem volutpat, enim lacinia imperdiet malesuada dictum enim justo ut inceptos.
-                hac quam luctus aenean platea sociosqu neque, eget vel elementum enim ut malesuada, turpis aliquam tortor curae sapien, aptent quam ut praesent tortor.
-                suscipit condimentum donec aenean ultricies tempor malesuada sollicitudin, vulputate sociosqu nullam conubia malesuada.
-            </p>
-            <p>
-                Nam rhoncus sapien netus felis mollis aptent placerat cursus congue, senectus aliquam gravida quam ligula massa nibh varius vulputate, lectus lacus aenean purus euismod amet convallis eleifend.
-                enim interdum dui nisl aptent vestibulum habitasse dolor eget, platea lorem velit maecenas malesuada magna vestibulum ut, ligula cras sodales vulputate porta sollicitudin dictum.
-                quam varius taciti himenaeos felis id proin morbi fermentum aliquam class, posuere blandit dui vulputate ac mi aliquam nunc leo nec, pulvinar ut sollicitudin magna litora conubia luctus hac quam.
-                odio molestie conubia metus curabitur facilisis magna viverra, sed suspendisse tempor platea sed pellentesque aptent, imperdiet tellus porta tortor venenatis potenti.
-            </p>
-            <p>
-                Lorem ipsum primis vestibulum pretium viverra porttitor potenti, ut inceptos elementum aenean netus orci mi eget, dolor fringilla faucibus himenaeos nisi mauris.
-                class quisque hendrerit leo eleifend neque pretium sit aenean, euismod praesent placerat potenti tempus suscipit lacinia, ullamcorper curabitur mattis curabitur congue aliquam turpis.
-                nunc lectus libero nam varius quisque libero elit imperdiet habitasse curabitur gravida rutrum, fusce nostra ut orci in vitae iaculis consequat integer amet.
-                integer curae malesuada interdum non interdum lorem bibendum vivamus, porttitor urna fames consequat porta nostra viverra ornare, lacus amet scelerisque integer vestibulum sed blandit.
-            </p>
-            <p>
-                Nisl sociosqu suspendisse magna risus eros primis etiam arcu turpis, commodo sociosqu mauris velit eros donec nam cras eleifend, consectetur urna arcu viverra neque cras praesent velit.
-                lobortis torquent ornare litora torquent convallis lectus feugiat, ornare vulputate velit volutpat justo proin nostra nibh, gravida elementum lobortis blandit metus nullam.
-                egestas aliquam litora aliquam iaculis tempor etiam elit facilisis felis, interdum pellentesque venenatis auctor porttitor donec porttitor enim eleifend, leo in nec ut mauris massa commodo purus.
-                fringilla hendrerit scelerisque condimentum curabitur phasellus commodo dolor venenatis, quis convallis velit nullam sem etiam praesent urna accumsan, malesuada arcu lacinia justo semper nibh class.
-            </p>
-            <p>
-                Mauris volutpat varius maecenas erat congue dolor egestas cras, euismod risus volutpat aenean ante feugiat felis rutrum quisque, etiam cras vel lorem mattis nec neque.
-                maecenas rhoncus semper elit cubilia sapien mauris, a sed ligula sagittis libero urna ut, hac commodo porttitor phasellus ut.
-                vitae sed velit cubilia commodo, dapibus porttitor eros elementum volutpat, sollicitudin nec suspendisse.
-                sapien ut netus iaculis quis cubilia semper platea aenean sed lectus feugiat, platea vivamus auctor condimentum semper aenean per lectus mauris convallis, donec aenean tempus tortor leo pharetra conubia magna odio ante.
-                tincidunt adipiscing erat cubilia class quisque molestie pretium torquent, per luctus feugiat interdum taciti dolor fames erat convallis, scelerisque nec habitasse nam orci ac erat.
-            </p>
-            <p>
-                Cras erat sapien interdum nostra eu pellentesque phasellus volutpat libero eros at maecenas vitae, neque vel amet per ultrices eu himenaeos faucibus primis sapien suscipit.
-                felis ut dolor posuere malesuada rhoncus elementum nec pharetra pretium rhoncus, mi viverra vivamus fames justo velit himenaeos eget lorem volutpat, enim lacinia imperdiet malesuada dictum enim justo ut inceptos.
-                hac quam luctus aenean platea sociosqu neque, eget vel elementum enim ut malesuada, turpis aliquam tortor curae sapien, aptent quam ut praesent tortor.
-                suscipit condimentum donec aenean ultricies tempor malesuada sollicitudin, vulputate sociosqu nullam conubia malesuada.
-            </p>
-            <p>
-                Nam rhoncus sapien netus felis mollis aptent placerat cursus congue, senectus aliquam gravida quam ligula massa nibh varius vulputate, lectus lacus aenean purus euismod amet convallis eleifend.
-                enim interdum dui nisl aptent vestibulum habitasse dolor eget, platea lorem velit maecenas malesuada magna vestibulum ut, ligula cras sodales vulputate porta sollicitudin dictum.
-                quam varius taciti himenaeos felis id proin morbi fermentum aliquam class, posuere blandit dui vulputate ac mi aliquam nunc leo nec, pulvinar ut sollicitudin magna litora conubia luctus hac quam.
-                odio molestie conubia metus curabitur facilisis magna viverra, sed suspendisse tempor platea sed pellentesque aptent, imperdiet tellus porta tortor venenatis potenti.
-            </p> */}
+     <p>teste</p>
     </section>
   );
 }
