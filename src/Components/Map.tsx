590528f5905c50d9e18cd1c7f1726bbe71c89d07
Luan Araujo <luan.araujo.violin@gmail.com>
2021-10-05 14:35:17 -0300

    :tada: add filter!!!

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,13 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import pontos from "./pontos.json";
 
 import "./Styles/map.css";
 
 const Mapa = () => {
+  const [filtro, setFiltro] = useState("");
+
+  const pontoss = pontos.filter((pontos) => pontos.descricao?.includes(filtro));
+  const mudarFiltro = (arg: string) => setFiltro(arg);
+
   return (
     <div className="container">
-      <div id="divBottons"></div>
+      <div id="divBottons">
+        <button onClick={() => mudarFiltro("Papel")}>Papel</button>
+        <button onClick={() => mudarFiltro("Metal")}>Metal</button>
+        <button onClick={() => mudarFiltro("Óleos")}>Óleos</button>
+        <button onClick={() => mudarFiltro("Pilhas")}>Pilhas</button>
+        <button onClick={() => mudarFiltro("Eletrônicos")}>Eletrônicos</button>
+        <button onClick={() => mudarFiltro("Orgânicos")}>Orgânicos</button>
+        <button onClick={() => mudarFiltro("")}>Outros</button>
+      </div>
 
       <MapContainer
         id="mapa"
@@ -20,7 +33,7 @@ const Mapa = () => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {pontos.map((pontos) => (
+        {pontoss.map((pontos) => (
           <Marker position={[pontos.x, pontos.y]}>
             <Popup>
               <h5>{pontos.nome}</h5>
