ef7312232594243127b2b0e56ac499fb66f2b36c
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-10-24 14:39:43 -0300

    Revert "Revert "Merge remote-tracking branch 'origin/Desenvolvimento' into Implementation""
    This reverts commit 168d679d8247b663cc6013abd69c64ea272b651c.

---
+++ src/Components/lixoAzul.js
@@ -0,0 +1,15 @@
+import React from "react";
+import imglixo from "../assets/lixo azul.png";
+
+function Azul() {
+  return (
+    <img
+      src={imglixo}
+      alt="Reciclagem"
+      height="200px"
+      margin="10px"
+    />
+  );
+}
+
+export default Azul;
--- src/Pages/Dicas/Dicas.js
+++ src/Pages/Dicas/Dicas.js
@@ -1,49 +1,230 @@
 import React from "react";
 import Grafico from "../../Components/BarChart";
 import PieChart from "../../Components/PieChart";
+
+import LixoAmarelo from "../../Components/lixoAmarelo";
+import LixoAzul from "../../Components/lixoAzul";
+import LixoMarrom from "../../Components/lixoMarrom";
+import LixoVerde from "../../Components/lixoVerde";
+import LixoVermelho from "../../Components/lixoVermelho";
+// import { Container, Row, Col } from 'reactstrap';
+
 import "../../Pages/Dicas/dicas.css";
-
-function handleMouseOver(e) {
-  e.target.style.transform = "scale(1.02)";
-}
-
-function handleMouseOut(e) {
-  e.target.style.transform = "scale(1)";
-}
 
 export default function Dicas() {
   return (
-    <main>
-      <article className="divBarChart">
-        <Grafico />
-        <div className="divTexto">
-          <p>
-            texto texto texto texto texto texto texto texto texto texto texto
-            texto texto texto texto texto texto texto texto texto texto texto
-            texto texto texto texto texto texto texto texto texto texto texto
-            texto texto texto texto texto texto texto texto texto texto texto
-            texto texto texto texto texto texto texto
-          </p>
+    <section>
+      <div className="container">
+        <div className="row">
+          <div className="graf row">
+            <div className="col-xs-7 col-md-7 col-sm-12 grafico">
+              {<Grafico />}
+            </div>
+            <div className="col-xs-5 col-md-5 col-sm-12">
+              <p className="text-grafico">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus minima dolorem facere optio quam. Aliquam possimus ab
+                qui facere placeat magnam laboriosam quod temporibus iure autem,
+                exercitationem i usto! Laudantium, quod? Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Possimus minima dolorem
+                facere optio quam. Aliquam possimus ab qui facere placeat magnam
+                laboriosam quod temporibus iure autem, exercitationem i usto!
+                Laudantium, quod? Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Possimus minima dolorem facere optio quam.
+                Aliquam possimus ab qui facere placeat magnam laboriosam quod
+                temporibus iure autem, exercitationem i usto! Laudantium, quod?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus minima dolorem facere optio quam. Aliquam possimus ab
+                qui facere placeat magnam laboriosam quod temporibus iure autem,
+                exercitationem i usto! Laudantium, quod?
+              </p>
+            </div>
+          </div>
         </div>
-      </article>
-      <div className="linha"></div>
-      <article className="divPieChart">
-        <PieChart />
-        <div className="divTexto">
-          <p>
-            texto texto texto texto texto texto texto texto texto texto texto
-            texto texto texto texto texto texto texto texto texto texto texto
-            texto texto texto texto texto texto texto texto texto texto texto
-            texto texto texto texto texto texto texto texto texto texto texto
-            texto texto texto texto texto texto texto
-          </p>
+
+        <div className="row">
+          <div className="graf">
+            <div className="col-12 justify-content-center chart">
+              {<PieChart />}
+            </div>
+            <div className="col-12">
+              <p className="text-chart">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus minima dolorem facere optio quam. Aliquam possimus ab
+                qui facere placeat magnam laboriosam quod temporibus iure autem,
+                exercitationem i usto! Laudantium, quod? Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Possimus minima dolorem
+                facere optio quam. Aliquam possimus ab qui facere placeat magnam
+                laboriosam quod temporibus iure autem, exercitationem i usto!
+                Laudantium, quod? Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Possimus minima dolorem facere optio quam.
+                Aliquam possimus ab qui facere placeat magnam laboriosam quod
+                temporibus iure autem, exercitationem i usto! Laudantium, quod?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus minima dolorem facere optio quam. Aliquam possimus ab
+                qui facere placeat magnam laboriosam quod temporibus iure autem,
+                exercitationem i usto! Laudantium, quod?
+              </p>
+            </div>
+          </div>
         </div>
-      </article>
-      <article className="textos">
-        <p>textos textos textos textos textos </p>
-        <p>textos textos textos textos textos </p>
-      </article>
-      <section className="sectionCards"></section>
-    </main>
+      </div>
+
+      <div className="container text-mid">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          minima dolorem facere optio quam. Aliquam possimus ab qui facere
+          placeat magnam laboriosam quod temporibus iure autem, exercitationem i
+          usto! Laudantium, quod?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          minima dolorem facere optio quam. Aliquam possimus ab qui facere
+          placeat magnam laboriosam quod temporibus iure autem, exercitationem i
+          usto! Laudantium, quod?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          minima dolorem facere optio quam. Aliquam possimus ab qui facere
+          placeat magnam laboriosam quod temporibus iure autem, exercitationem i
+          usto! Laudantium, quod?
+        </p>
+        <div className="row">
+          <div className="col-2"></div>
+          <div className="col-10 vl">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              minima dolorem facere optio quam. Aliquam possimus ab qui facere
+              placeat magnam laboriosam quod temporibus iure autem,
+              exercitationem i usto! Laudantium, quod?
+            </p>
+          </div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          minima dolorem facere optio quam. Aliquam possimus ab qui facere
+          placeat magnam laboriosam quod temporibus iure autem, exercitationem i
+          usto! Laudantium, quod?
+        </p>
+      </div>
+
+      <div className="dicas d-flex">
+        <div className="container">
+          <div className="external">
+            <div className="row">
+              <div className="col-12">
+                <div className="row">
+                  <div className="col-2 div-img d-flex justify-content-center">
+                    <div className="img-dica">{<LixoAmarelo />}</div>
+                  </div>
+                  <div className="col-1"></div>
+                  <div className="col-9">
+                    <p className="text-dicas">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Possimus minima dolorem facere optio quam. Aliquam
+                      possimus ab qui facere placeat magnam laboriosam quod
+                      temporibus iure autem, exercitationem i usto! Laudantium,
+                      quod?Lorem ipsum dolor sit amet consectetur adipisicing
+                      elit. Possimus minima dolorem facere optio quam. Aliquam
+                      possimus ab qui facere placeat magnam laboriosam quod
+                      temporibus iure autem, exercitationem i usto! Laudantium,
+                      quod?
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <hr className="hr" />
+              <div className="col-12">
+                <div className="row">
+                  <div className="col-9">
+                    <p className="text-dicas">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Possimus minima dolorem facere optio quam. Aliquam
+                      possimus ab qui facere placeat magnam laboriosam quod
+                      temporibus iure autem, exercitationem i usto! Laudantium,
+                      quod?Lorem ipsum dolor sit amet consectetur adipisicing
+                      elit. Possimus minima dolorem facere optio quam. Aliquam
+                      possimus ab qui facere placeat magnam laboriosam quod
+                      temporibus iure autem, exercitationem i usto! Laudantium,
+                      quod?
+                    </p>
+                  </div>
+                  <div className="col-1"></div>
+                  <div className="col-2 div-img d-flex justify-content-center">
+                    <div className="img-dica">{<LixoAzul />}</div>
+                  </div>
+                </div>
+              </div>
+              <hr className="hr" />
+              <div className="col-12">
+                <div className="row">
+                  <div className="col-2 div-img img-dica d-flex justify-content-center">
+                    <div className="img-dica">{<LixoMarrom />}</div>
+                  </div>
+                  <div className="col-1"></div>
+                  <div className="col-9">
+                    <p className="text-dicas">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Possimus minima dolorem facere optio quam. Aliquam
+                      possimus ab qui facere placeat magnam laboriosam quod
+                      temporibus iure autem, exercitationem i usto! Laudantium,
+                      quod?Lorem ipsum dolor sit amet consectetur adipisicing
+                      elit. Possimus minima dolorem facere optio quam. Aliquam
+                      possimus ab qui facere placeat magnam laboriosam quod
+                      temporibus iure autem, exercitationem i usto! Laudantium,
+                      quod?
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <hr className="hr" />
+              <div className="col-12">
+                <div className="row">
+                  <div className="col-9">
+                    <p className="text-dicas">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Possimus minima dolorem facere optio quam. Aliquam
+                      possimus ab qui facere placeat magnam laboriosam quod
+                      temporibus iure autem, exercitationem i usto! Laudantium,
+                      quod?Lorem ipsum dolor sit amet consectetur adipisicing
+                      elit. Possimus minima dolorem facere optio quam. Aliquam
+                      possimus ab qui facere placeat magnam laboriosam quod
+                      temporibus iure autem, exercitationem i usto! Laudantium,
+                      quod?
+                    </p>
+                  </div>
+                  <div className="col-1"></div>
+                  <div className="col-2 div-img d-flex justify-content-center">
+                    <div className="img-dica">{<LixoVerde />}</div>
+                  </div>
+                </div>
+              </div>
+              <hr className="hr" />
+              <div className="col-12">
+                <div className="row">
+                  <div className="col-2 div-img d-flex justify-content-center">
+                    <div className="img-dica">{<LixoVermelho />}</div>
+                  </div>
+                  <div className="col-1"></div>
+                  <div className="col-9">
+                    <p className="text-dicas">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Possimus minima dolorem facere optio quam. Aliquam
+                      possimus ab qui facere placeat magnam laboriosam quod
+                      temporibus iure autem, exercitationem i usto! Laudantium,
+                      quod?Lorem ipsum dolor sit amet consectetur adipisicing
+                      elit. Possimus minima dolorem facere optio quam. Aliquam
+                      possimus ab qui facere placeat magnam laboriosam quod
+                      temporibus iure autem, exercitationem i usto! Laudantium,
+                      quod?
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
