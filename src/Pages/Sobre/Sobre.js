f24c38ab5078faba0097478669c8f15925194cf5
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-11-02 17:56:06 -0300

    Update Sobre.js
    Criando os cards dos participantes

--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -1,11 +1,143 @@
 import React from "react";
+import { Row, Container, Col } from "reactstrap";
+import "./sobre.css";
 
 export default function sobre(props) {
   return (
     <section>
-     <p>teste</p>
-    <p>outro teste</p>    
-  
+      <Container>
+        <Row xs="1" md="3" sm="3">
+          <Col className="bg-light border">
+            <div className="CardPartc1">
+              <div className="RedondPt1"></div>
+            </div>
+            <div className="CardPartc1">
+              Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+              scelerisque hac donec vitae, eleifend consequat elementum justo
+              suscipit libero nisl donec malesuada sapien suscipit ultrices
+              fusce, ut faucibus nam est vel vivamus condimentum tincidunt
+              senectus neque porttitor. tempus vivamus integer dictumst sit
+              tristique ac eleifend odio eros, platea dapibus enim tempor per
+              fermentum scelerisque suscipit, erat pellentesque justo orci in
+              aptent purus netus. egestas himenaeos ullamcorper malesuada
+              senectus pretium aenean, vivamus at tincidunt lacinia cubilia
+              luctus proin, vulputate sodales dapibus tellus quisque. tristique
+              facilisis risus ut mollis commodo fermentum aenean neque, in fusce
+              id dictumst potenti vivamus aptent sodales mattis, porttitor per
+              felis curabitur donec ornare interdum.
+            </div>
+          </Col>
+          <br />
+          <Col className="bg-light border">
+            <div className="CardPartc1">
+              <div className="RedondPt1"></div>
+            </div>
+            <div className="CardPartc1">
+              Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+              scelerisque hac donec vitae, eleifend consequat elementum justo
+              suscipit libero nisl donec malesuada sapien suscipit ultrices
+              fusce, ut faucibus nam est vel vivamus condimentum tincidunt
+              senectus neque porttitor. tempus vivamus integer dictumst sit
+              tristique ac eleifend odio eros, platea dapibus enim tempor per
+              fermentum scelerisque suscipit, erat pellentesque justo orci in
+              aptent purus netus. egestas himenaeos ullamcorper malesuada
+              senectus pretium aenean, vivamus at tincidunt lacinia cubilia
+              luctus proin, vulputate sodales dapibus tellus quisque. tristique
+              facilisis risus ut mollis commodo fermentum aenean neque, in fusce
+              id dictumst potenti vivamus aptent sodales mattis, porttitor per
+              felis curabitur donec ornare interdum.
+            </div>
+          </Col>
+          <br />
+          <Col className="bg-light border">
+            <div className="CardPartc1">
+              <div className="RedondPt1"></div>
+            </div>
+            <div className="CardPartc1">
+              Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+              scelerisque hac donec vitae, eleifend consequat elementum justo
+              suscipit libero nisl donec malesuada sapien suscipit ultrices
+              fusce, ut faucibus nam est vel vivamus condimentum tincidunt
+              senectus neque porttitor. tempus vivamus integer dictumst sit
+              tristique ac eleifend odio eros, platea dapibus enim tempor per
+              fermentum scelerisque suscipit, erat pellentesque justo orci in
+              aptent purus netus. egestas himenaeos ullamcorper malesuada
+              senectus pretium aenean, vivamus at tincidunt lacinia cubilia
+              luctus proin, vulputate sodales dapibus tellus quisque. tristique
+              facilisis risus ut mollis commodo fermentum aenean neque, in fusce
+              id dictumst potenti vivamus aptent sodales mattis, porttitor per
+              felis curabitur donec ornare interdum.
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <br />
+      <Container>
+        <Row xs="1" md="3" sm="3">
+          <Col className="bg-light border">
+            <div className="CardPartc1">
+              <div className="RedondPt1"></div>
+            </div>
+            <div className="CardPartc1">
+              Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+              scelerisque hac donec vitae, eleifend consequat elementum justo
+              suscipit libero nisl donec malesuada sapien suscipit ultrices
+              fusce, ut faucibus nam est vel vivamus condimentum tincidunt
+              senectus neque porttitor. tempus vivamus integer dictumst sit
+              tristique ac eleifend odio eros, platea dapibus enim tempor per
+              fermentum scelerisque suscipit, erat pellentesque justo orci in
+              aptent purus netus. egestas himenaeos ullamcorper malesuada
+              senectus pretium aenean, vivamus at tincidunt lacinia cubilia
+              luctus proin, vulputate sodales dapibus tellus quisque. tristique
+              facilisis risus ut mollis commodo fermentum aenean neque, in fusce
+              id dictumst potenti vivamus aptent sodales mattis, porttitor per
+              felis curabitur donec ornare interdum.
+            </div>
+          </Col>
+          <br />
+          <Col className="bg-light border">
+            <div className="CardPartc1">
+              <div className="RedondPt1"></div>
+            </div>
+            <div className="CardPartc1">
+              Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+              scelerisque hac donec vitae, eleifend consequat elementum justo
+              suscipit libero nisl donec malesuada sapien suscipit ultrices
+              fusce, ut faucibus nam est vel vivamus condimentum tincidunt
+              senectus neque porttitor. tempus vivamus integer dictumst sit
+              tristique ac eleifend odio eros, platea dapibus enim tempor per
+              fermentum scelerisque suscipit, erat pellentesque justo orci in
+              aptent purus netus. egestas himenaeos ullamcorper malesuada
+              senectus pretium aenean, vivamus at tincidunt lacinia cubilia
+              luctus proin, vulputate sodales dapibus tellus quisque. tristique
+              facilisis risus ut mollis commodo fermentum aenean neque, in fusce
+              id dictumst potenti vivamus aptent sodales mattis, porttitor per
+              felis curabitur donec ornare interdum.
+            </div>
+          </Col>
+          <br />
+          <Col className="bg-light border">
+            <div className="CardPartc1">
+              <div className="RedondPt1"></div>
+            </div>
+            <div className="CardPartc1">
+              Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+              scelerisque hac donec vitae, eleifend consequat elementum justo
+              suscipit libero nisl donec malesuada sapien suscipit ultrices
+              fusce, ut faucibus nam est vel vivamus condimentum tincidunt
+              senectus neque porttitor. tempus vivamus integer dictumst sit
+              tristique ac eleifend odio eros, platea dapibus enim tempor per
+              fermentum scelerisque suscipit, erat pellentesque justo orci in
+              aptent purus netus. egestas himenaeos ullamcorper malesuada
+              senectus pretium aenean, vivamus at tincidunt lacinia cubilia
+              luctus proin, vulputate sodales dapibus tellus quisque. tristique
+              facilisis risus ut mollis commodo fermentum aenean neque, in fusce
+              id dictumst potenti vivamus aptent sodales mattis, porttitor per
+              felis curabitur donec ornare interdum.
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 }
