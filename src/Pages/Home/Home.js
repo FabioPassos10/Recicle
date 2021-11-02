d3acd5b3cdc133d395d4d9f0ba7b2b4f5d6d9f1f
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-11-02 17:56:36 -0300

    Update Home.js
    criando os card dos lixos

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from "react";
 import Cards from "../../Components/Card";
 import "../../Components/Styles/card.css";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import Mapa from "../../Components/Map.tsx";
 import "./home.css";
 
@@ -17,7 +17,147 @@ export default function Home(props) {
       </div>
       <div id="Triangulo"></div>
       <div id="DivCard_Home">
-        <div id="CardDent"></div>
+        <div id="CardDent">
+          <Container fluid={true} style={{ paddingTop: "200px" }}>
+            <Row xs="1" md="3" sm="3">
+              <Col className="bg-light border">
+                <div className="CardPartc1">
+                  <div className="RedondPt1"></div>
+                </div>
+                <div className="CardPartc1">
+                  Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+                  scelerisque hac donec vitae, eleifend consequat elementum
+                  justo suscipit libero nisl donec malesuada sapien suscipit
+                  ultrices fusce, ut faucibus nam est vel vivamus condimentum
+                  tincidunt senectus neque porttitor. tempus vivamus integer
+                  dictumst sit tristique ac eleifend odio eros, platea dapibus
+                  enim tempor per fermentum scelerisque suscipit, erat
+                  pellentesque justo orci in aptent purus netus. egestas
+                  himenaeos ullamcorper malesuada senectus pretium aenean,
+                  vivamus at tincidunt lacinia cubilia luctus proin, vulputate
+                  sodales dapibus tellus quisque. tristique facilisis risus ut
+                  mollis commodo fermentum aenean neque, in fusce id dictumst
+                  potenti vivamus aptent sodales mattis, porttitor per felis
+                  curabitur donec ornare interdum.
+                </div>
+              </Col>
+              <br />
+              <Col className="bg-light border">
+                <div className="CardPartc1">
+                  <div className="RedondPt1"></div>
+                </div>
+                <div className="CardPartc1">
+                  Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+                  scelerisque hac donec vitae, eleifend consequat elementum
+                  justo suscipit libero nisl donec malesuada sapien suscipit
+                  ultrices fusce, ut faucibus nam est vel vivamus condimentum
+                  tincidunt senectus neque porttitor. tempus vivamus integer
+                  dictumst sit tristique ac eleifend odio eros, platea dapibus
+                  enim tempor per fermentum scelerisque suscipit, erat
+                  pellentesque justo orci in aptent purus netus. egestas
+                  himenaeos ullamcorper malesuada senectus pretium aenean,
+                  vivamus at tincidunt lacinia cubilia luctus proin, vulputate
+                  sodales dapibus tellus quisque. tristique facilisis risus ut
+                  mollis commodo fermentum aenean neque, in fusce id dictumst
+                  potenti vivamus aptent sodales mattis, porttitor per felis
+                  curabitur donec ornare interdum.
+                </div>
+              </Col>
+              <br />
+              <Col className="bg-light border">
+                <div className="CardPartc1">
+                  <div className="RedondPt1"></div>
+                </div>
+                <div className="CardPartc1">
+                  Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+                  scelerisque hac donec vitae, eleifend consequat elementum
+                  justo suscipit libero nisl donec malesuada sapien suscipit
+                  ultrices fusce, ut faucibus nam est vel vivamus condimentum
+                  tincidunt senectus neque porttitor. tempus vivamus integer
+                  dictumst sit tristique ac eleifend odio eros, platea dapibus
+                  enim tempor per fermentum scelerisque suscipit, erat
+                  pellentesque justo orci in aptent purus netus. egestas
+                  himenaeos ullamcorper malesuada senectus pretium aenean,
+                  vivamus at tincidunt lacinia cubilia luctus proin, vulputate
+                  sodales dapibus tellus quisque. tristique facilisis risus ut
+                  mollis commodo fermentum aenean neque, in fusce id dictumst
+                  potenti vivamus aptent sodales mattis, porttitor per felis
+                  curabitur donec ornare interdum.
+                </div>
+              </Col>
+            </Row>
+          </Container>
+          <br />
+          <Container fluid={true}>
+            <Row xs="1" md="3" sm="3">
+              <Col className="bg-light border">
+                <div className="CardPartc1">
+                  <div className="RedondPt1"></div>
+                </div>
+                <div className="CardPartc1">
+                  Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+                  scelerisque hac donec vitae, eleifend consequat elementum
+                  justo suscipit libero nisl donec malesuada sapien suscipit
+                  ultrices fusce, ut faucibus nam est vel vivamus condimentum
+                  tincidunt senectus neque porttitor. tempus vivamus integer
+                  dictumst sit tristique ac eleifend odio eros, platea dapibus
+                  enim tempor per fermentum scelerisque suscipit, erat
+                  pellentesque justo orci in aptent purus netus. egestas
+                  himenaeos ullamcorper malesuada senectus pretium aenean,
+                  vivamus at tincidunt lacinia cubilia luctus proin, vulputate
+                  sodales dapibus tellus quisque. tristique facilisis risus ut
+                  mollis commodo fermentum aenean neque, in fusce id dictumst
+                  potenti vivamus aptent sodales mattis, porttitor per felis
+                  curabitur donec ornare interdum.
+                </div>
+              </Col>
+              <br />
+              <Col className="bg-light border">
+                <div className="CardPartc1">
+                  <div className="RedondPt1"></div>
+                </div>
+                <div className="CardPartc1">
+                  Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+                  scelerisque hac donec vitae, eleifend consequat elementum
+                  justo suscipit libero nisl donec malesuada sapien suscipit
+                  ultrices fusce, ut faucibus nam est vel vivamus condimentum
+                  tincidunt senectus neque porttitor. tempus vivamus integer
+                  dictumst sit tristique ac eleifend odio eros, platea dapibus
+                  enim tempor per fermentum scelerisque suscipit, erat
+                  pellentesque justo orci in aptent purus netus. egestas
+                  himenaeos ullamcorper malesuada senectus pretium aenean,
+                  vivamus at tincidunt lacinia cubilia luctus proin, vulputate
+                  sodales dapibus tellus quisque. tristique facilisis risus ut
+                  mollis commodo fermentum aenean neque, in fusce id dictumst
+                  potenti vivamus aptent sodales mattis, porttitor per felis
+                  curabitur donec ornare interdum.
+                </div>
+              </Col>
+              <br />
+              <Col className="bg-light border">
+                <div className="CardPartc1">
+                  <div className="RedondPt1"></div>
+                </div>
+                <div className="CardPartc1">
+                  Urna quam curabitur fermentum ullamcorper tortor metus ut arcu
+                  scelerisque hac donec vitae, eleifend consequat elementum
+                  justo suscipit libero nisl donec malesuada sapien suscipit
+                  ultrices fusce, ut faucibus nam est vel vivamus condimentum
+                  tincidunt senectus neque porttitor. tempus vivamus integer
+                  dictumst sit tristique ac eleifend odio eros, platea dapibus
+                  enim tempor per fermentum scelerisque suscipit, erat
+                  pellentesque justo orci in aptent purus netus. egestas
+                  himenaeos ullamcorper malesuada senectus pretium aenean,
+                  vivamus at tincidunt lacinia cubilia luctus proin, vulputate
+                  sodales dapibus tellus quisque. tristique facilisis risus ut
+                  mollis commodo fermentum aenean neque, in fusce id dictumst
+                  potenti vivamus aptent sodales mattis, porttitor per felis
+                  curabitur donec ornare interdum.
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     </>
   );
