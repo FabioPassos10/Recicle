1d076e4382971420b210ed66d0f904de8de6f484
Aluno <>
2021-11-08 15:21:58 -0300

    Atualização Sobre Nós
    Texto inicial adicionado

--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -6,6 +6,34 @@ export default function sobre(props) {
   return (
     <section>
       <Container>
+        <Row>
+          <Col>
+            <h3></h3>
+
+            <p>
+              O projeto ReCircle é originário do desenvolvimento de um Trabalho de Conclusão de Curso
+              (TCC) realizado por alunos do 3º ano do Curso Técnico em Desenvolvimento de Sistemas Integrado ao Ensino Médio
+              (3º EMDS) ministrado na instituição de ensino ETEC Pedro Ferreira Alves – Mogi Mirim, no ano de 2021.
+          </p>
+            <p>
+              Partindo-se de uma ideia para benefício social direcionada ao município e alinhada à aprendizagem adquirida na área de
+              desenvolvimento web, surge ReCircle: um projeto que objetiva a conscientização popular, o compartilhamento de informações e
+              divulgação de organizações comunitárias e pontos de descarte da região, visando o auxílio à preservação da natureza.
+          </p>
+            <p>
+              Para execução deste projeto, a equipe foi organizada, atuando de forma colaborativa em diferentes etapas de seu
+              desenvolvimento. Inicialmente, sua proposta foi apresentada por João Pedro e, então, a prototipagem ocorreu em responsabilidade
+              de Fábio Eloy. O design foi elaborado por Alexandre Batista e Camile Russo e a programação, aplicada por Luan Henrique,
+              Luiz Gustavo, Fábio e Alexandre. Para produção de conteúdos textuais ao site, pesquisas e escrita foram realizadas por Camile,
+              Luiz e João Pedro.
+          </p>
+            <p>
+              Apesar da divisão de tarefas, o time de desenvolvimento prestou auxílio a todos os âmbitos deste trabalho de maneira conjunta.
+          </p><br></br>
+
+            <h4>EQUIPE</h4>
+          </Col>
+        </Row>
         <Row xs="1" md="3" sm="3">
           <Col className="bg-light border">
             <div className="CardPartc1">
