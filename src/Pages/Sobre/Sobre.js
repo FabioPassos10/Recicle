da8853436627d6168419ae8de3e97d9d9e932190
João Pedro Vaz <pp.joao.mg@hotmail.com>
2021-11-15 09:42:42 -0300

    atualização de branch
    atualizei toda a branch com um merg branch e após isso coloquei o conteúdo

--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -7,22 +7,34 @@ export default function sobre(props) {
     <section>
       <Container>
         <Row>
-          <Col >
+          <Col>
+            <h3></h3>
+
             <p>
-              Lorem ipsum dapibus felis neque sociosqu magna vestibulum velit dictumst sodales pharetra enim, integer et a blandit fusce viverra eros quis suspendisse elementum habitant.
-              conubia ornare vitae facilisis egestas lacus venenatis sem augue arcu magna phasellus platea sem imperdiet elementum curae ante, maecenas arcu iaculis vestibulum augue vestibulum varius semper volutpat placerat sollicitudin volutpat vestibulum semper ad.
-              mi vivamus a non condimentum sed eu purus malesuada dapibus consectetur, nec tincidunt cursus cubilia lacinia vestibulum facilisis hendrerit gravida, etiam ad neque vestibulum commodo sagittis imperdiet est quisque.
-              curae consectetur justo inceptos morbi dictum augue quisque, purus accumsan mollis posuere placerat rhoncus, curabitur posuere lacus mattis condimentum senectus.
-</p>
+              O projeto ReCircle é originário do desenvolvimento de um Trabalho de Conclusão de Curso
+              (TCC) realizado por alunos do 3º ano do Curso Técnico em Desenvolvimento de Sistemas Integrado ao Ensino Médio
+              (3º EMDS) ministrado na instituição de ensino ETEC Pedro Ferreira Alves – Mogi Mirim, no ano de 2021.
+          </p>
             <p>
-              Venenatis habitasse eleifend tempor pharetra ligula metus blandit lacus, quam iaculis magna dapibus libero auctor congue dolor auctor, posuere aptent conubia felis fusce duis odio.
-              ante nisi porttitor proin libero tortor ligula fermentum porttitor massa, lobortis ut risus odio arcu potenti molestie tellus sociosqu adipiscing, sociosqu curae habitant cubilia urna erat rutrum velit.
-              mollis ut semper mauris malesuada nisi nec luctus inceptos sodales, vestibulum turpis inceptos fusce sociosqu etiam massa torquent, velit arcu duis fringilla ante habitasse congue phasellus.
-              morbi ad auctor cubilia sagittis eleifend, nam netus nostra mi non, class sed mattis blandit.
-</p>
+              Partindo-se de uma ideia para benefício social direcionada ao município e alinhada à aprendizagem adquirida na área de
+              desenvolvimento web, surge ReCircle: um projeto que objetiva a conscientização popular, o compartilhamento de informações e
+              divulgação de organizações comunitárias e pontos de descarte da região, visando o auxílio à preservação da natureza.
+          </p>
+            <p>
+              Pa ra execução deste projeto, a equipe foi organizada, atuando de forma colaborativa em diferentes etapas de seu
+              desenvolvimento. Inicialmente, sua proposta foi apresentada por João Pedro e, então, a prototipagem ocorreu em responsabilidade
+              de Fábio Eloy. O design foi elaborado por Alexandre Batista e Camile Russo e a programação, aplicada por Luan Henrique,
+              Luiz Gustavo, Fábio e Alexandre. Para produção de conteúdos textuais ao site, pesquisas e escrita foram realizadas por Camile,
+              Luiz e João Pedro.
+          </p>
+            <p>
+              Apesar da divisão de tarefas, o time de desenvolvimento prestou auxílio a todos os âmbitos deste trabalho de maneira conjunta.
+          </p><br></br>
+
+            <h4>EQUIPE</h4>
           </Col>
         </Row>
-      </Container>
+        </Container>
 
       <Container fluid={true} >
         <Row xs="1" md="2" sm="1" xl="3" lg="3">
